6fad1b909d7fcb98aee89ac1187c90d312154ae0
tuong <lovesunshine269@gmail.com>
2021-09-23 21:47:16 +0700

    Initial stage

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    //std::io::Result<()>{
+  /*   dotenv::ok();
+
+    env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set"); */
+}
